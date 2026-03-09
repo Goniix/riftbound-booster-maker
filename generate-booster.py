0586e3f53732d8f5440796c580cd4d6f8ab17e8b
Gabriel Redouin-Innecco <87310572+Goniix@users.noreply.github.com>
2026-03-09 02:09:23 +0100

    new using codex api - booster generator

--- generate-booster.py
+++ generate-booster.py
@@ -1,4 +1,3 @@
-from enum import Enum
 import sqlite3
 import requests
 import random
@@ -17,34 +16,92 @@ import random
 #showcase: 1/72~ => any fullart
 #signed: 1/720~ (osef)
 
-def getCommonList(set: str, cursor: sqlite3.Cursor):
-    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND rarity = 'Common' AND type = 'REG'",set)
+def getCommonList(setID: str, cursor: sqlite3.Cursor):
+    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND rarity = 'Common' AND type = 'REG'", (setID,))
     cards = res.fetchall()
     return cards
 
-def getUncommonList(set: str, cursor: sqlite3.Cursor):
-    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND rarity = 'Uncommon' AND type = 'REG'",set)
+def getUncommonList(setID: str, cursor: sqlite3.Cursor):
+    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND rarity = 'Uncommon'", (setID,))
     cards = res.fetchall()
     return cards
 
-def getRareList(set: str, cursor: sqlite3.Cursor):
-    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND rarity = 'Rare' AND type = 'REG'",set)
+def getRareList(setID: str, cursor: sqlite3.Cursor):
+    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND rarity = 'Rare'", (setID,))
+    cards = res.fetchall()
+    return cards
+
+def getEpicList(setID: str, cursor: sqlite3.Cursor):
+    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND rarity = 'Epic'", (setID,))
+    cards = res.fetchall()
+    return cards
+
+def getAltList(setID: str, cursor: sqlite3.Cursor):
+    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND type = 'ALT'", (setID,))
+    cards = res.fetchall()
+    return cards
+
+def getOverList(setID: str, cursor: sqlite3.Cursor):
+    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND type = 'OVR'", (setID,))
+    cards = res.fetchall()
+    return cards
+
+def getSignedList(setID: str, cursor: sqlite3.Cursor):
+    res = cursor.execute("SELECT * FROM cards WHERE setID = ? AND type = 'SGN'", (setID,))
+    cards = res.fetchall()
+    return cards
+
+def getTokenList(setID: str, cursor: sqlite3.Cursor):
+    res = cursor.execute("SELECT * FROM cards WHERE setID = ?  AND type = 'TKN'", (setID,))
     cards = res.fetchall()
     return cards
     
 def pickCount(cards : list[str], count : int):
+    if count <0:
+        return []
     random.shuffle(cards)
     return cards[:count]
         
-    
-
-def genBooster(set : str):
+def genBooster(setID : str):
     availableSets = ["OGN", "SFD", "OGS"]
     con = sqlite3.connect("cards.db")
     cursor = con.cursor()
     
-    commons = pickCount(getCommonList(set, cursor),7)
-    print(commons)
+    commons = pickCount(getCommonList(setID, cursor),7)
+    uncommons = pickCount(getUncommonList(setID, cursor),3)
+    
+    flexList = pickCount(getUncommonList(setID, cursor),3) + pickCount(getRareList(setID, cursor),1)
+    random.shuffle(flexList)
+    flex = flexList[1:]
+    
+    
+    epic = random.randrange(0,4) == 0
+    alt = random.randrange(0,12) == 0
+    over= random.randrange(0,72) == 0
+    signed = random.randrange(0,720) == 0
+    
+    special: list[str] = []
+    if epic:
+        special += pickCount(getEpicList(setID, cursor),1)
+    if alt:
+        special += pickCount(getAltList(setID, cursor),1)
+    if over:
+        special += pickCount(getOverList(setID, cursor),1)
+    if signed:
+        special += pickCount(getSignedList(setID, cursor),1)
+    
+    rares =  pickCount(getRareList(setID, cursor),2 - len(special)) + special
+        
+        
+    token = pickCount(getTokenList(setID, cursor),1)
+    
+    booster = commons + uncommons + flex + rares + token
+    
+    for card in booster:
+        try:
+            print(f"{card[1]} | {card[2]}")
+        except:
+            print(card)
     
 genBooster("OGN")
     
--- generate-db.py
+++ generate-db.py
@@ -3,27 +3,11 @@ import requests
 import re
     
 def getType(cardId: str)->str:
+    cardId = cardId.upper()
     tokens = ["OGN-271", "OGN-272", "OGN-273", "OGN-274", "OGN-XXX"]
-    if cardId in tokens or re.search(".*-[TR][0-9]+$",cardId) != None:
+    runes = ["OGN-007", "OGN-007a", "OGN-042", "OGN-042a", "OGN-089", "OGN-089a", "OGN-126", "OGN-126a", "OGN-166", "OGN-166a", "OGN-214", "OGN-214a"]
+    if cardId in tokens or cardId in runes or re.search(".*-[TR][0-9]+$",cardId) != None:
         return "TKN"
-    
-    if re.search(".*-[0-9]{3}[aA]$",cardId) != None:
-        return "ALT"
-    
-    try:
-        spl = cardId.split("-")
-        cardSet = str(spl[0])
-        number = int(spl[1])
-        match cardSet:
-            case 'OGN':
-                if number > 298:
-                    return "OVR"
-            case 'SFD':
-                if number > 221:
-                    return "OVR"
-    except:
-        pass
-
     return "REG"
     
         
@@ -32,53 +16,64 @@ def getType(cardId: str)->str:
 def isPromoCard(cardId: str, cardName: str) -> bool:
     return cardId.endswith("-p") or cardId.endswith("-P") or cardId.startswith("ARC") or cardName.endswith("Promo)") or cardName.endswith("(Oversized)")
 
-def isSignedCard(cardId: str)-> bool:
-    return cardId.endswith("-STAR")
-
-def isOgnRuneCard(cardId: str)->bool:
-    ognRunes = ["OGN-007", "OGN-007a", "OGN-042", "OGN-042a", "OGN-089", "OGN-089a", "OGN-126", "OGN-126a", "OGN-166", "OGN-166a", "OGN-214", "OGN-214a"]
-    return cardId in ognRunes
-
 def updateDatabase():
     con = sqlite3.connect("cards.db")
-
     cursor = con.cursor()
-
-    print("dropping table")
-    cursor.execute("DROP TABLE IF EXISTS cards")
-    print("creating table")
-    cursor.execute("CREATE TABLE cards(id VARCHAR, name VARCHAR, rarity VARCHAR, image VARCHAR, setID CHAR(3), type VARCHAR)")
-    print("fetching cards")
-    res = requests.get("https://api.dotgg.gg/cgfw/getcards?game=riftbound&mode=plain")
     
-    if(res.status_code != 200):
-        print(f"Error detected : status_code is {res.status_code}")
-        return
-    
-    print("reading json")    
-    resJson = res.json()
-    
-    if(resJson == None):
-        print("error : couldn't read json")
-    
-    for elem in resJson:
-        cardId = str(elem["id"])
-        cardName = str(elem["name"])
+    try:
+        print("dropping table")
+        cursor.execute("DROP TABLE IF EXISTS cards")
+        cursor.execute("DROP TABLE IF EXISTS removed")
+        print("creating table")
+        cursor.execute("CREATE TABLE cards(id VARCHAR, name VARCHAR, rarity VARCHAR, image VARCHAR, setID CHAR(3), type VARCHAR)")
+        cursor.execute("CREATE TABLE removed(id VARCHAR, name VARCHAR, rarity VARCHAR, image VARCHAR, setID CHAR(3), type VARCHAR)")
+        print("fetching cards")
+        res = requests.get("https://api.riftcodex.com/cards?size=100")
         
-        isPromo = isPromoCard(cardId, cardName)
-        isSigned = isSignedCard(cardId)
-        ognRune = isOgnRuneCard(cardId)
+        if(res.status_code != 200):
+            print(f"Error detected : status_code is {res.status_code}")
+            return
         
-        cardSet = cardId.split("-")[0]
+        resJson = res.json()
         
+        if(resJson == None):
+            print("error : couldn't read json")
         
-        if(not isPromo and not isSigned and not ognRune):
-            cardType = str(getType(cardId))
-            cursor.execute("INSERT INTO cards VALUES (?, ?, ?, ?, ?, ?)", (cardId, cardName, elem["rarity"], elem["image"], cardSet, cardType))
-    
-    cursor.close()
-    con.commit()
-    con.close()
+        pagecount = resJson["pages"]
+        
+        for i in range(1,pagecount+1):
+            resJson = requests.get(f"https://api.riftcodex.com/cards?page={i}&size=100").json()
+            
+            cardList = resJson["items"]
+            for card in cardList:
+                print(card)
+                cardId = "-".join(str(card["riftbound_id"]).split("-")[:2])
+                cardName = str(card["name"])
+                cardRarity = str(card["classification"]["rarity"])
+                cardArtUrl = str(card["media"]["image_url"])
+                
+                isSigned = bool(card["metadata"]["signature"])
+                isAlternate = bool(card["metadata"]["alternate_art"])
+                isOvernumbered = bool(card["metadata"]["overnumbered"])
+                
+                cardSet = str(card["set"]["set_id"])
+                
+                cardType = str(getType(cardId))
+                if isAlternate: 
+                    cardType = "ALT"
+                if isOvernumbered:
+                    cardType = "OVR"
+                if isSigned:
+                    cardType = "SGN"
+                
+                cursor.execute("INSERT INTO cards VALUES (?, ?, ?, ?, ?, ?)", (cardId, cardName, cardRarity, cardArtUrl, cardSet, cardType))
+                    
+            
+        con.commit()
+        
+    finally:
+        cursor.close()
+        con.close()
         
         
 
